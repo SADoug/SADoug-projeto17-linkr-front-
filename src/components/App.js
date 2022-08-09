916cf71a7297243d9231e4c9a96b4c6fe12f826a
SADoug <douglastec2@gmail.com>
2022-08-09 09:31:25 -0300

    template inicial

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+function App() {
+  
+  return (
+    <>
+    <p>Testando o App 2</p>
+    </>
+  )
+}
+
+export default App;
